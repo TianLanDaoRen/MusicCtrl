cfb36d23c8fbf2b5d83de55e6e6905f3baead103
EliotZhang <eliotzhang7000@gmail.com>
2023-10-06 11:30:25 +0800

    fix: cleancode fixes
    author: TianLanDaoRen <eliotzhang7000@gmail.com>

--- MCController.xaml.cs
+++ MCController.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
@@ -13,17 +8,17 @@ namespace MusicCtrl
 	/// MCController.xaml 的交互逻辑
 	/// </summary>
 	public partial class MCController : System.Windows.Controls.UserControl
-    {
-		MusicCtrl Master;
+	{
+		private MusicCtrl master;
 
 		[DllImport("user32.dll", SetLastError = true)]
 		static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
 
-		public MCController(MusicCtrl master)
+		public MCController(MusicCtrl mst)
 		{
 			InitializeComponent();
-			Master = master;
-			Master.MW.Main.UIGrid.Children.Insert(0, this);
+			master = mst;
+			master.MW.Main.UIGrid.Children.Insert(0, this);
 			Opacity = 0.85;
 			Margin = new Thickness(0, 500, 0, 0);
 		}
@@ -58,15 +53,15 @@ namespace MusicCtrl
 			keybd_event((byte)Keys.VolumeDown, 0, 0, 0);
 		}
 
-		public void Show(object sender=null, RoutedEventArgs e=null)
+		public void Show(object sender = null, RoutedEventArgs e = null)
 		{
-			Master.Settings.ShowWindow = 1;
+			master.Settings.ShowWindow = 1;
 			Visibility = Visibility.Visible;
 		}
 
 		public void Hide(object sender = null, RoutedEventArgs e = null)
 		{
-			Master.Settings.ShowWindow = 0;
+			master.Settings.ShowWindow = 0;
 			Visibility = Visibility.Hidden;
 		}
 	}
